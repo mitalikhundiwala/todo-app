ea865d193a0c6f4649f5b8516694fba32add34f0
Mitali Patel <mitali.khundiwala@gmail.com>
2020-02-23 12:20:30 +0400

    added tests for reducers

---
+++ src/tests/fixtures/todos.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+        userId: 1,
+        todoId: 1,
+        title: 'delectus aut autem',
+        completed: true
+    },
+    {
+        userId: 2,
+        todoId: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false
+    },
+    {
+        userId: 2,
+        todoId: 3,
+        title: 'fugiat veniam minus',
+        completed: false
+    }
+];
--- src/tests/reducers/users.reducer.test.ts
+++ src/tests/reducers/users.reducer.test.ts
@@ -1,6 +1,6 @@
 import usersReducer from '../../reducers/users.reducer';
 import { setInitialData } from '../../actions/app.action';
-import data from '../../data/db.json';
+import data from "../../data/db.json";
 
 test('should set default state', () => {
     const state = usersReducer(undefined, { type: '@@INIT' });
